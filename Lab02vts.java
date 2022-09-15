dd9c85377e1446da9ee755f4cd1d994e7c995e9b
dom <dominic.cimaglia@vtsd.com>
2022-09-15 12:07:33 -0400

    Lab 2

---
+++ Lab02vts.java
@@ -0,0 +1,19 @@
+public class Lab02vts {
+    public static void main (String[] args)
+    {
+        System.out.println("lab 02, 100 Point Version");
+        System.out.println();
+        System.out.println();
+        System.out.println("Measure today's knowledge  ");
+        System.out.println(" by yesterdasys conufsion");
+        System.out.println();
+        System.out.println("Bewilderment + exposure = Obvious");
+        System.out.println();
+        System.out.println("# ##### ##### \t ##### ##### # #     # ##### #####");
+        System.out.println("# #   # #   # \t #   # #   # # ##    #   #   #");
+        System.out.println("# #   # #   # \t ##### #   # # # #   #   #   #####");
+        System.out.println("# #   # #   # \t #     #   # # #  #  #   #       #");
+        System.out.println("# ##### ##### \t #     ##### # #   ##    #   #####");
+
+    }
+}
